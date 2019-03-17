b09daeeb69370b9781dffa75593953b0300bb3ed
Ohyeahxiaocong <48181612+Ohyeahxiaocong@users.noreply.github.com>
2019-03-18 00:24:41 +0800

    Create code0.py

---
+++ Hackathon 3.x/GBDT/code0.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 22:47:39 2019
+
+@author: Hyomin
+"""
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import cross_validation, metrics
+from sklearn.grid_search import GridSearchCV
+
+import matplotlib.pylab as plt
+
+# 将训练集分为输入和输出
+train = pd.read_csv('datasets/train_modified.csv')
+x_colums = [x for x in train.columns if x not in ['Disbursed']]
+X = train[x_colums]
+y = train['Disbursed']
