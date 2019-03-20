3f5b73460575b9b141d66c400cbab4c4d0f2c1e4
Ohyeahxiaocong <48181612+Ohyeahxiaocong@users.noreply.github.com>
2019-03-21 00:11:03 +0800

    Update GBDT.py

--- Hackathon 3.x/GBDT.py
+++ Hackathon 3.x/GBDT.py
@@ -18,10 +18,10 @@ y = dataset['Disbursed']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
 
 # 用默认参数跑模型
-#train_accuracy:0.985319
+#train_accuracy:0.985305
 #test_accuracy:0.985635
-#train_auc:0.897560
-#test_auc:0.840123
+#train_auc:0.886390
+#test_auc:0.842154
 gbm0 = GradientBoostingClassifier(loss='deviance',
                                   learning_rate=0.1,
                                   n_estimators=100,
@@ -46,11 +46,11 @@ print('test_accuracy:%.6f' % test_accuracy)
 print('train_auc:%.6f' % train_auc)
 print('test_auc:%.6f' % test_auc)
 
-# 网格搜索最优迭代次数 {'n_estimators': 150} 
-#train_accuracy:0.985348
+# 网格搜索最优迭代次数 {'n_estimators': 140} 
+#train_accuracy:0.985319
 #test_accuracy:0.985635
-#train_auc:0.907980
-#test_auc:0.841166
+#train_auc:0.893536
+#test_auc:0.843665
 gbm1 = GradientBoostingClassifier(loss='deviance',
                                   learning_rate=0.1,
                                   n_estimators=100,
@@ -58,7 +58,6 @@ gbm1 = GradientBoostingClassifier(loss='deviance',
                                   min_samples_split=500,
                                   min_samples_leaf=50,
                                   max_depth=5,
-                                  max_leaf_nodes=50,
                                   max_features='sqrt',
                                   random_state=10)
 parameter1 = {'n_estimators': np.arange(120, 200, 10)}
@@ -80,22 +79,21 @@ print('test_accuracy:%.6f' % test_accuracy)
 print('train_auc:%.6f' % train_auc)
 print('test_auc:%.6f' % test_auc)
 
-# 树最大深度和最小分类样本树的最优组合 {'max_depth': 3, 'min_samples_split': 300}
-#train_accuracy:0.985420
-#test_accuracy:0.985578
-#train_auc:0.890614
-#test_auc:0.845169
+# 树最大深度和最小分类样本树的最优组合 {'max_depth': 7, 'min_samples_split': 900}
+#train_accuracy:0.985334
+#test_accuracy:0.985635
+#train_auc:0.912968
+#test_auc:0.846042
 gbm2 = GradientBoostingClassifier(loss='deviance',
                                   learning_rate=0.1,
-                                  n_estimators=150,
+                                  n_estimators=140,
                                   subsample=0.8,
                                   min_samples_split=500,
                                   min_samples_leaf=50,
                                   max_depth=5,
-                                  max_leaf_nodes=50,
                                   max_features='sqrt',
                                   random_state=10)
-parameter2 = {'max_depth': range(3, 11, 2), 'min_samples_split': range(100, 1100, 200)}
+parameter2 = {'max_depth': range(3, 11, 2), 'min_samples_split': range(100, 1300, 200)}
 gsearch2 = GridSearchCV(estimator=gbm2, param_grid=parameter2, scoring='roc_auc',
                         n_jobs=4, iid=False, cv=5)
 gsearch2.fit(X_train, y_train)
@@ -114,27 +112,53 @@ print('test_accuracy:%.6f' % test_accuracy)
 print('train_auc:%.6f' % train_auc)
 print('test_auc:%.6f' % test_auc)
 
-# 最小分类样本数和最小叶节点样本数的最优组合 {'min_samples_leaf': 80, 'min_samples_split': 1300} 0.849653033147489
-gbm3 = GradientBoostingClassifier(learning_rate=0.1,
-                                  n_estimators=100,
+# 最小分类样本数和最小叶节点样本数的最优组合 {'min_samples_leaf': 50}
+#train_accuracy:0.985334
+#test_accuracy:0.985635
+#train_auc:0.912968
+#test_auc:0.846042
+gbm3 = GradientBoostingClassifier(loss='deviance',
+                                  learning_rate=0.1,
+                                  n_estimators=140,
+                                  subsample=0.8,
+                                  min_samples_split=900,
+                                  min_samples_leaf=50,
                                   max_depth=7,
                                   max_features='sqrt',
-                                  subsample=0.8,
                                   random_state=10)
-parameter3 = {'min_samples_leaf': range(60, 130, 20), 'min_samples_split': range(100, 1500, 150)}
+parameter3 = {'min_samples_leaf': range(30, 120, 20)}
 gsearch3 = GridSearchCV(estimator=gbm3, param_grid=parameter3, scoring='roc_auc',
                         n_jobs=4, iid=False, cv=5)
 gsearch3.fit(X_train, y_train)
 gsearch3.best_score_
 gsearch3.best_params_
 
-# 最大特征数调优 {'max_features': 25} 0.8498905820022624
-gbm4 = GradientBoostingClassifier(learning_rate=0.1,
-                                  n_estimators=100,
-                                  max_depth=7,
-                                  min_samples_leaf=80,
-                                  min_samples_split=1300,
+y_train_pred = gsearch3.predict(X_train)
+y_test_pred = gsearch3.predict(X_test)
+y_train_predprob = gsearch3.predict_proba(X_train)
+y_test_predprob = gsearch3.predict_proba(X_test)
+train_accuracy = metrics.accuracy_score(y_train, y_train_pred)
+test_accuracy = metrics.accuracy_score(y_test, y_test_pred)
+train_auc = metrics.roc_auc_score(y_train, y_train_predprob[:, 1])
+test_auc = metrics.roc_auc_score(y_test, y_test_predprob[:, 1])
+print('train_accuracy:%.6f' % train_accuracy)
+print('test_accuracy:%.6f' % test_accuracy)
+print('train_auc:%.6f' % train_auc)
+print('test_auc:%.6f' % test_auc)
+
+# 最大特征数调优 {'max_features': 10}
+#train_accuracy:0.985363
+#test_accuracy:0.985635
+#train_auc:0.917098
+#test_auc:0.847754
+gbm4 = GradientBoostingClassifier(loss='deviance',
+                                  learning_rate=0.1,
+                                  n_estimators=140,
                                   subsample=0.8,
+                                  min_samples_split=900,
+                                  min_samples_leaf=50,
+                                  max_depth=7,
+                                  max_features='sqrt',
                                   random_state=10)
 parameter4 = {'max_features': range(10, 31, 5)}
 gsearch4 = GridSearchCV(estimator=gbm4, param_grid=parameter4, scoring='roc_auc',
@@ -143,30 +167,65 @@ gsearch4.fit(X_train, y_train)
 gsearch4.best_score_
 gsearch4.best_params_
 
-# 采样率调优 {'subsample': 0.85} 0.8507789563605644
-gbm5 = GradientBoostingClassifier(learning_rate=0.1,
-                                  n_estimators=100,
+y_train_pred = gsearch4.predict(X_train)
+y_test_pred = gsearch4.predict(X_test)
+y_train_predprob = gsearch4.predict_proba(X_train)
+y_test_predprob = gsearch4.predict_proba(X_test)
+train_accuracy = metrics.accuracy_score(y_train, y_train_pred)
+test_accuracy = metrics.accuracy_score(y_test, y_test_pred)
+train_auc = metrics.roc_auc_score(y_train, y_train_predprob[:, 1])
+test_auc = metrics.roc_auc_score(y_test, y_test_predprob[:, 1])
+print('train_accuracy:%.6f' % train_accuracy)
+print('test_accuracy:%.6f' % test_accuracy)
+print('train_auc:%.6f' % train_auc)
+print('test_auc:%.6f' % test_auc)
+
+# 采样率调优 {'subsample': 0.85}
+#train_accuracy:0.985391
+#test_accuracy:0.985578
+#train_auc:0.916803
+#test_auc:0.843284
+gbm5 = GradientBoostingClassifier(loss='deviance',
+                                  learning_rate=0.1,
+                                  n_estimators=140,
+                                  subsample=0.8,
+                                  min_samples_split=900,
+                                  min_samples_leaf=50,
                                   max_depth=7,
-                                  min_samples_leaf=80,
-                                  min_samples_split=1300,
-                                  max_features=25,
+                                  max_features=10,
                                   random_state=10)
 parameter5 = {'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9]}
 gsearch5 = GridSearchCV(estimator=gbm5, param_grid=parameter5, scoring='roc_auc',
                         n_jobs=4, iid=False, cv=5)
+gsearch5.fit(X_train, y_train)
 gsearch5.best_score_
 gsearch5.best_params_
 
+y_train_pred = gsearch5.predict(X_train)
+y_test_pred = gsearch5.predict(X_test)
+y_train_predprob = gsearch5.predict_proba(X_train)
+y_test_predprob = gsearch5.predict_proba(X_test)
+train_accuracy = metrics.accuracy_score(y_train, y_train_pred)
+test_accuracy = metrics.accuracy_score(y_test, y_test_pred)
+train_auc = metrics.roc_auc_score(y_train, y_train_predprob[:, 1])
+test_auc = metrics.roc_auc_score(y_test, y_test_predprob[:, 1])
+print('train_accuracy:%.6f' % train_accuracy)
+print('test_accuracy:%.6f' % test_accuracy)
+print('train_auc:%.6f' % train_auc)
+print('test_auc:%.6f' % test_auc)
+
 # 验证调优后的参数 auc:0.906034684455213
-gbm6 = GradientBoostingClassifier(learning_rate=0.1,
-                                  n_estimators=100,
+gbm6 = GradientBoostingClassifier(loss='deviance',
+                                  learning_rate=0.1,
+                                  n_estimators=140,
+                                  subsample=0.75,
+                                  min_samples_split=900,
+                                  min_samples_leaf=50,
                                   max_depth=7,
-                                  min_samples_leaf=80,
-                                  min_samples_split=1300,
-                                  max_features=25,
-                                  subsample=0.85,
+                                  max_features=10,
                                   random_state=10)
 gbm6.fit(X_train,y_train)
+
 y_pred = gbm6.predict(X_train)
 y_predprob = gbm6.predict_proba(X_train)
 metrics.accuracy_score(y_train,y_pred)
