0db900e2aaa9cc1f2bc8e511b9761b078dcf08c1
OHYEAHxiaocong <wangzx@chianoly.com>
2019-04-02 17:52:09 +0800

    Update xgboost.py

--- Happiness/xgboost.py
+++ Happiness/xgboost.py
@@ -9,6 +9,8 @@ import numpy as np
 import xgboost as xgb
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+import matplotlib.pyplot as plt
 
 # 训练集和验证集
 dataset = pd.read_csv('dataset/train_modified.csv')
@@ -16,8 +18,21 @@ test_dataset = pd.read_csv('dataset/test_modified.csv')
 x_colums = [x for x in dataset.columns if x not in ['id','happiness']]
 X = dataset[x_colums]
 y = dataset['happiness']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
+X_resampled_smote, y_resampled_smote = SMOTE().fit_sample(X, y)
+X_resampled_smote = pd.DataFrame(X_resampled_smote,columns=X.columns)
+X_train, X_test, y_train, y_test = train_test_split(X_resampled_smote, y_resampled_smote,
+                                                    test_size=0.3, random_state=10)
 test_data = test_dataset[x_colums]
+
+xgr = xgb.XGBRegressor()
+xgr.fit(X, y)
+feature_importances = pd.DataFrame()
+feature_importances['col'] = X.columns
+feature_importances['imp'] = xgr.feature_importances_
+feature_importances.sort_values('imp', inplace=True, ascending=False)
+
+plt.bar(x=feature_importances['col'][:30], height=feature_importances['imp'][:30])
+
 
 a=[]
 b=[]
